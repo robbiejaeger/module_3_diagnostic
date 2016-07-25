9448ffb0560a5bb5eb9ffbc9e06d7c6b975f026e
Robbie Jaeger <jaeger.rob@gmail.com>
2016-07-25 11:18:47 -0600

    Able to search with optional distance Did not get to searching with distance and any type Wanted to break out the two different searches in the FuelStationService The search_by_zip was getting messy with if/esle statements

--- app/services/fuel_stations_service.rb
+++ app/services/fuel_stations_service.rb
@@ -5,8 +5,17 @@ class FuelStationsService
     @conn.headers["X-Api-Key"] = ENV["NREL_API_KEY"]
   end
 
-  def search_by_zip(params)
-    response = @conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{params[:zip]}&limit=10")
+  def search_by_zip(parameters)
+    response = @conn.get("/api/alt-fuel-stations/v1/nearest.json") do |req|
+      req.params['location'] = parameters[:zip]
+      req.params['limit'] = 10
+      if parameters[:distance]
+        req.params['radius'] = parameters[:distance]
+      else
+        req.params['radius'] = 6
+      end
+      req.params['fuel_type'] = "ELEC,LPG"
+    end
     parse(response)
   end
 
