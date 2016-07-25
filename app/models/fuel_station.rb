56008e0fb300c8464d7b8bfcc36a7246d6de28a9
Robbie Jaeger <jaeger.rob@gmail.com>
2016-07-25 10:22:15 -0600

    Add basic request structure, WIP

--- app/models/fuel_station.rb
+++ app/models/fuel_station.rb
@@ -1,3 +1,11 @@
 class FuelStation < OpenStruct
-  
+
+  def self.service(params)
+    FuelStationsService.new(params)
+  end
+
+  def self.search_by_zip(params)
+    raw_stations = service(params).search_by_zip
+    byebug
+  end
 end
--- app/services/fuel_stations_service.rb
+++ app/services/fuel_stations_service.rb
@@ -1,6 +1,17 @@
 class FuelStationsService
 
-  def initialize
-    
+  def initialize(params)
+    @params = params
+    @conn = Faraday.new(:url => 'https://api.data.gov/nrel')
+    @conn.headers["X-Api-Key"] = ENV["NREL_API_KEY"]
+  end
+
+  def search_by_zip
+    response = @conn.get "/alt-fuel-stations/v1/nearest.json?location=#{@params[:zip]}&limit=10"
+    parse(response)
+  end
+
+  def parse(response)
+    JSON.parse(response.body)
   end
 end
